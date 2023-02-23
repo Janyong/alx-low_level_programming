e1b0d7237609b30886145e2012fb3468e85342ed
Janyong <molishojanyong@gmail.com>
2023-02-22 17:12:32 -0800

    Mult

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_times_table - Prints the times table of the input,
- * Starting with 0.
+ * starting with 0.
  * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
@@ -18,6 +18,8 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 				prod = num * mult;
+				if (prod <= 99)
+					_putchar(' ');
 				if (prod <= 9)
 					_putchar(' ');
 				if (prod >= 100)
